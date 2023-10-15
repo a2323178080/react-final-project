6ef266af10c6fdc81a2c97c194003ac2412f23c4
lai <a2323178080@gmail.com>
2023-10-16 01:12:40 +0800

    調整pagination，刪除不必要程式碼

--- src/views/admin/admin-product/admin-products.js
+++ src/views/admin/admin-product/admin-products.js
@@ -124,27 +124,8 @@ export default function AdminProducts() {
             </tbody>
         </table>
 
+        <div className="mt-5">
         <Pagination current={current} onChange={switchPage} total={parseInt(pagination.total_pages, 10) * 10}/>
-
-        <nav aria-label='Page navigation example'>
-            <ul className='pagination'>
-                <li className='page-item'>
-                    <a className='page-link disabled' href='/' aria-label='Previous'>
-                        <span aria-hidden='true'>&laquo;</span>
-                    </a>
-                </li>
-                {[...new Array(5)].map((_, i) => (// eslint-disable-next-line react/no-array-index-key
-                    <li className='page-item' key={`${i}_page`}>
-                        <a className={`page-link ${i + 1 === 1 && 'active'}`} href='/'>
-                            {i + 1}
-                        </a>
-                    </li>))}
-                <li className='page-item'>
-                    <a className='page-link' href='/' aria-label='Next'>
-                        <span aria-hidden='true'>&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+        </div>
     </div>)
 }
