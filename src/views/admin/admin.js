b070ff697731480bd90a97a4fc04bdbbe452bc62
lai <a2323178080@gmail.com>
2023-10-15 13:46:03 +0800

    admin路由微調

--- src/views/admin/admin.js
+++ src/views/admin/admin.js
@@ -1,7 +1,8 @@
-import {Outlet,useNavigate} from 'react-router-dom'
+import {Outlet, useNavigate} from 'react-router-dom'
 import axios from 'axios';
-import { useEffect } from 'react';
-export default function Admin(){
+import {useEffect} from 'react';
+
+export default function Admin() {
     const navigate = useNavigate();
     const logout = () => {
         document.cookie = 'hexToken=;';
@@ -31,71 +32,71 @@ export default function Admin(){
         })();
     }, [navigate, token]);
 
-    return(
-    <div>
-        <nav className='navbar navbar-expand-lg bg-dark'>
-            <div className='container-fluid'>
-                <p className='text-white mb-0'>HEX EATS 後台管理系統</p>
-                <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-bs-toggle='collapse'
-                    data-bs-target='#navbarNav'
-                    aria-controls='navbarNav'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                >
-                    <span className='navbar-toggler-icon' />
-                </button>
-                <div
-                    className='collapse navbar-collapse justify-content-end'
-                    id='navbarNav'
-                >
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'>
-                            <button
-                                type='button'
-                                className='btn btn-sm btn-light'
-                                onClick={logout}
-                            >登出
-                            </button>
-                        </li>
+    return (
+        <div>
+            <nav className='navbar navbar-expand-lg bg-dark'>
+                <div className='container-fluid'>
+                    <p className='text-white mb-0'>HEX EATS 後台管理系統</p>
+                    <button
+                        className='navbar-toggler'
+                        type='button'
+                        data-bs-toggle='collapse'
+                        data-bs-target='#navbarNav'
+                        aria-controls='navbarNav'
+                        aria-expanded='false'
+                        aria-label='Toggle navigation'
+                    >
+                        <span className='navbar-toggler-icon'/>
+                    </button>
+                    <div
+                        className='collapse navbar-collapse justify-content-end'
+                        id='navbarNav'
+                    >
+                        <ul className='navbar-nav'>
+                            <li className='nav-item'>
+                                <button
+                                    type='button'
+                                    className='btn btn-sm btn-light'
+                                    onClick={logout}
+                                >登出
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className='d-flex' style={{minHeight: 'calc(100vh - 56px)'}}>
+                <div className='bg-light' style={{width: '200px'}}>
+                    <ul className='list-group list-group-flush'>
+                        <a
+                            className='list-group-item list-group-item-action py-3'
+                            to='/admin/products'
+                        >
+                            <i className='bi bi-cup-fill me-2'/>
+                            產品列表
+                        </a>
+                        <a
+                            className='list-group-item list-group-item-action py-3'
+                            to='/admin/coupons'
+                        >
+                            <i className='bi bi-ticket-perforated-fill me-2'/>
+                            優惠卷列表
+                        </a>
+                        <a
+                            className='list-group-item list-group-item-action py-3'
+                            to='/admin/orders'
+                        >
+                            <i className='bi bi-receipt me-2'/>
+                            訂單列表
+                        </a>
                     </ul>
                 </div>
-            </div>
-        </nav>
-        <div className='d-flex' style={{ minHeight: 'calc(100vh - 56px)' }}>
-            <div className='bg-light' style={{ width: '200px' }}>
-                <ul className='list-group list-group-flush'>
-                    <a
-                        className='list-group-item list-group-item-action py-3'
-                        to='/admin/products'
-                    >
-                        <i className='bi bi-cup-fill me-2' />
-                        產品列表
-                    </a>
-                    <a
-                        className='list-group-item list-group-item-action py-3'
-                        to='/admin/coupons'
-                    >
-                        <i className='bi bi-ticket-perforated-fill me-2' />
-                        優惠卷列表
-                    </a>
-                    <a
-                        className='list-group-item list-group-item-action py-3'
-                        to='/admin/orders'
-                    >
-                        <i className='bi bi-receipt me-2' />
-                        訂單列表
-                    </a>
-                </ul>
-            </div>
-            <div className='w-100'>
-                {/* Products */}
-                { token && <Outlet />}
-                {/* Products end */}
+                <div className='w-100'>
+                    {/* Products */}
+                    {token && <Outlet/>}
+                    {/* Products end */}
+                </div>
             </div>
         </div>
-    </div>
     )
 }
