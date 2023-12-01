3fc20d574469eaa8c2a1f289885f907975627223
lai <a2323178080@gmail.com>
2023-12-01 11:01:20 +0800

    all 安裝bootstrap-icon;antd-icon

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
