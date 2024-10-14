741a3f5c3ac9d90b8a0ab9548e16b94eabc1d3ea
Eason Lai <eason024@jcg-tech.com>
2024-10-14 11:26:22 +0800

    因為vercel 調整

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import "./assets/index.scss";
 import {HashRouter} from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
+import App from './app'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
